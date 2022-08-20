96ec857f5f0a137487b5e7773be95727f2c01e1d
Ali Khedmati <Ali@khedmati.ir>
2022-08-20 22:10:05 +0430

    Been working on Finnotech.

--- config/kyc.php
+++ config/kyc.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'providers' =>  [
+    'drivers' =>  [
         'finnotech' =>  [
             'base-url'  =>  env('KYC_FINNOTECH_BASE_URL', 'https://apibeta.finnotech.ir/'),
             'client-id' =>  env('KYC_FINNOTECH_CLIENT_ID'),
--- src/Drivers/Finnotech.php
+++ src/Drivers/Finnotech.php
@@ -2,7 +2,50 @@
 
 namespace Alikhedmati\Kyc\Drivers;
 
+use GuzzleHttp\Client;
+
 class Finnotech
 {
+    /**
+     * @var string
+     */
 
+    private string $restApiBase;
+
+    public function __construct()
+    {
+        $this->restApiBase = config('kyc.drivers.finnotech.base-url');
+    }
+
+    public function getAccessToken(): string
+    {
+        /**
+         * The Strategy.
+         */
+    }
+
+    /**
+     * @param bool $isAuthenticated
+     * @return Client
+     */
+
+    private function client(bool $isAuthenticated = false): Client
+    {
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        if ($isAuthenticated){
+
+            $headers['Authorization'] = $this->getAccessToken();
+
+        }
+
+        return new Client([
+            'base_uri'  =>  $this->restApiBase,
+            'headers'   =>  $headers,
+            'http_errors'   =>  false
+        ]);
+    }
 }
